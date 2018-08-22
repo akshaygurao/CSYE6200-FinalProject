1b209b430c699a526724bff9d60a6ed9557222a7
Akshay <gurao.a@husky.neu.edu>
2018-08-22 15:17:06 -0400

    Adding UI

--- src/edu/neu/csye6200/Driver.java
+++ src/edu/neu/csye6200/Driver.java
@@ -5,5 +5,4 @@ public class Driver {
 	public static void main(String[] args) {
 		TemperatureController.demo();
 	}
-
 }
---
+++ src/edu/neu/csye6200/ui/Controller.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.neu.csye6200.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author akshaygurao
+ */
+public class Controller {
+    private String event = "True";
+    private int currTemp = 66;
+    private int setTemp = 71;
+    private List<String> rooms;
+
+    public Controller() {
+        this.rooms = new ArrayList<>();
+        this.rooms.add("BedRoom");
+        this.rooms.add("Living Room");
+    }
+
+    public List<String> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<String> rooms) {
+        this.rooms = rooms;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public int getCurrTemp() {
+        return currTemp;
+    }
+
+    public void setCurrTemp(int currTemp) {
+        this.currTemp = currTemp;
+    }
+
+    public int getSetTemp() {
+        return setTemp;
+    }
+
+    public void setSetTemp(int setTemp) {
+        this.setTemp = setTemp;
+    }
+    
+    
+    public static void demo(){
+        
+        
+    }
+}
